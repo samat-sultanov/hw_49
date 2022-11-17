1827d8776bac8e5371d7659bcccee4bedc451c81
samat_sultanov <samatsayak@gmail.com>
2022-11-18 02:15:03 +0600

    создал модели задачи, статуса, типа; создал и сделал миграцию; зарегистрировал все модели админ-панели; создал разные типы и статусы

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Task, Status, Type
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['summary', 'type', 'created_at']
+    list_display_links = ['summary']
+    list_filter = ['status', 'type']
+    search_fields = ['summary', 'description']
+    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['task_status']
+    list_display_links = ['task_status']
+    fields = ['task_status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['task_type']
+    list_display_links = ['task_type']
+    fields = ['task_type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status, StatusAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        abstract = True
+
+
+class Task(BaseModel):
+    summary = models.CharField(max_length=100, null=False, blank=False, verbose_name='Заголовок')
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name="Описание")
+    status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, related_name='tasks',
+                               verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT, related_name='tasks',
+                             verbose_name='Тип')
+
+    def __str__(self):
+        return f"{self.id}. {self.summary}: {self.status}"
+
+    class Meta:
+        db_table = "tasks"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
+
+class Status(BaseModel):
+    task_status = models.CharField(max_length=30, null=False, blank=False, verbose_name='Статус задачи')
+
+    def __str__(self):
+        return f"{self.task_status}"
+
+    class Meta:
+        db_table = "statuses"
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
+
+
+class Type(BaseModel):
+    task_type = models.CharField(max_length=30, null=False, blank=False, verbose_name='Тип задачи')
+
+    def __str__(self):
+        return f"{self.task_type}"
+
+    class Meta:
+        db_table = "types"
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
