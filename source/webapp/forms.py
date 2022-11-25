e40d5f366f12baf6bf676a07a3a4910e99297930
samat_sultanov <samatsayak@gmail.com>
2022-11-25 14:38:38 +0600

    написал функцию для валидации описания задачи в webapp.forms

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
 
 from webapp.models import Task
@@ -12,3 +13,11 @@ class TaskForm(forms.ModelForm):
             'type': widgets.CheckboxSelectMultiple,
             'description': widgets.Textarea(attrs={'placeholder': 'Описание Задачи'})
         }
+
+    def clean(self):
+        sql_keywords = ['select', 'delete', 'insert', 'update', 'create']
+        summary = self.cleaned_data.get('summary')
+        description = self.cleaned_data.get('description')
+        if description:
+            if len(summary) > len(description):
+                raise ValidationError('Описание задачи не может быть меньше заголовка!')
