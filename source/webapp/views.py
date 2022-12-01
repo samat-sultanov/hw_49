acc7e1414880b4093916516faa805d3217c0b238
samat_sultanov <samatsayak@gmail.com>
2022-12-02 00:24:49 +0600

    прописал дополнительные методы в классе IndexView для работы с поиском, добавил поиск в шаблон pagination.html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,9 +1,11 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils.http import urlencode
 from django.views import View
 
 from django.views.generic import TemplateView, ListView
 
-from webapp.forms import TaskForm
+from webapp.forms import TaskForm, SearchForm
 from webapp.models import Task
 
 
@@ -13,6 +15,33 @@ class IndexView(ListView):
     context_object_name = 'tasks'
     ordering = ['-updated_at']
     paginate_by = 4
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        if self.search_value:
+            return Task.objects.filter(
+                Q(summary__icontains=self.search_value) | Q(description__icontains=self.search_value))
+        return Task.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            query = urlencode({'search': self.search_value})
+            context['query'] = query
+            context['search'] = self.search_value
+        return context
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get('search')
 
 
 class TaskView(TemplateView):
