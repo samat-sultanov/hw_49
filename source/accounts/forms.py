89da0fb14036b9927a4c6c7826ecce3a887f7080
samat_sultanov <samatsayak@gmail.com>
2022-12-20 01:07:02 +0600

    добавил валидатор first_name, last_name в форму MyUserCreationForm в accounts.forms

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -7,8 +7,10 @@ class MyUserCreationForm(UserCreationForm):
     email = forms.CharField(max_length=30, required=True, label='Email')
 
     def clean(self):
-        if self.cleaned_data["first_name"].strip() == '' and self.cleaned_data["last_name"].strip() == '':
-            raise ValidationError("Одно из полей (first_name, last_name) должно быть заполнено")
+        cleaned_data = super().clean()
+        if cleaned_data['first_name'].strip() == '' and cleaned_data['last_name'].strip() == '':
+            raise ValidationError('Одно из полей (first_name, last_name) должно быть заполнено!')
+        return cleaned_data
 
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
