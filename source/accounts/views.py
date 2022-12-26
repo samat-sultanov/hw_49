55988eb9b318e395b81ddf1b72070137f8389838
samat_sultanov <samatsayak@gmail.com>
2022-12-27 00:28:58 +0600

    создал представление и шаблон для личной страницы пользователя

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,19 +1,21 @@
 from django.contrib.auth import login
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.forms import MyUserCreationForm
+from accounts.models import Profile
 
 
 class RegisterView(CreateView):
-    model = User
+    model = get_user_model()
     template_name = 'registration.html'
     form_class = MyUserCreationForm
 
     def form_valid(self, form):
         user = form.save()
+        Profile.objects.create(user=user)
         login(self.request, user)
         return redirect(self.get_success_url())
 
@@ -24,3 +26,17 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('webapp:index')
         return next_url
+
+
+class UserDetailView(DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        print(self.get_object().profile)
+        print(self.get_object().profile.url_github)
+        print(self.get_object().profile.about)
+        return super().get_context_data(**kwargs)
+
+
