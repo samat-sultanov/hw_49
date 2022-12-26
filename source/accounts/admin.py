05de1d3a6de9486ddebd1ae876e91fae28a0c20a
samat_sultanov <samatsayak@gmail.com>
2022-12-26 22:51:41 +0600

    настроил урлы для аватарок, зарегистрировал модель профиля в админке

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from accounts.models import Profile
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['url_github', 'avatar', 'about']
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+User = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
