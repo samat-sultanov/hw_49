3877441ecf3a0e195b380350599e7fc24350ddb1
samat_sultanov <samatsayak@gmail.com>
2023-01-06 12:37:03 +0600

    создал формы для изменения данных юзера: UserChangeForm, ProfileChangeForm

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+
+from accounts.models import Profile
 
 
 class MyUserCreationForm(UserCreationForm):
@@ -14,3 +17,16 @@ class MyUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
+
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+        labels = {'first_name': 'Имя', 'last_name': 'Фамилия', 'email': 'Email'}
+
+
+class ProfileChangeForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['url_github', 'avatar', 'about']
